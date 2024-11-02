b96a499e5aa0ebd6abb3a0f77692fd2283304b83
Jake Daynes <jake@whitebox.solutions>
2024-11-01 17:23:06 -0700

    Commented out extra free

--- shared/gimxinput/src/hid/hidinput.c
+++ shared/gimxinput/src/hid/hidinput.c
@@ -151,7 +151,7 @@ int hidinput_init(const GPOLL_INTERFACE * poll_interface, int(*callback)(GE_Even
                             };
                             if (ghid_register(device->hid, device, &callbacks) < 0) {
                                 close_device(device);
-                                free(device);
+                                // free(device);
                             } else {
                                 GLIST_ADD(hidinput_devices, device);
                             }
